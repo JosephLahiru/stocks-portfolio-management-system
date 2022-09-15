b04594c7b6439cd54ab0010ec5c874f311725a21
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-09-16 00:11:57 +0530

    Retrieved data from username and password and added a Toast

--- app/src/main/java/com/example/stocksportfoliomanagementsystem/LoginActivity.java
+++ app/src/main/java/com/example/stocksportfoliomanagementsystem/LoginActivity.java
@@ -3,12 +3,31 @@ package com.example.stocksportfoliomanagementsystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button loginButton;
+    EditText userNameEditText;
+    EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        userNameEditText = (EditText) findViewById(R.id.editTextTextUserName);
+        passwordEditText = (EditText) findViewById(R.id.editTextTextPassword);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginActivity.this, "User Name : " + userNameEditText.getText().toString() + "\nPassword : " + passwordEditText.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
