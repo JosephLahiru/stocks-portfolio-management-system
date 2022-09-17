1530984ba088f8b72f3f3df4964b80f5d12e78b2
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-09-18 00:13:31 +0530

    Stooped at SignInActivity

--- app/src/main/java/com/example/stocksportfoliomanagementsystem/ProfileActivity.java
+++ app/src/main/java/com/example/stocksportfoliomanagementsystem/ProfileActivity.java
@@ -2,13 +2,33 @@ package com.example.stocksportfoliomanagementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    Button btnLogout;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        btnLogout = findViewById(R.id.logoutButton);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null) {
+            startActivity(new Intent(ProfileActivity.this, LoginActivity.class));
+        }
     }
 }
--- app/src/main/java/com/example/stocksportfoliomanagementsystem/SignInActivity.java
+++ app/src/main/java/com/example/stocksportfoliomanagementsystem/SignInActivity.java
@@ -1,13 +1,53 @@
 package com.example.stocksportfoliomanagementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignInActivity extends AppCompatActivity {
+
+    EditText userName, email, password, passwordConfirm;
+    FirebaseAuth mAuth;
+    Button signInButton;
+    TextView loginText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        userName = findViewById(R.id.editTextUserName);
+        email = findViewById(R.id.etEmail);
+        password = findViewById(R.id.editTextPassword);
+        passwordConfirm = findViewById(R.id.editTextPasswordConfirm);
+        signInButton = (Button) findViewById(R.id.signInButton);
+        loginText = (TextView) findViewById(R.id.loginText);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createUser();
+            }
+        });
+
+        loginText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignInActivity.this, LoginActivity.class));
+            }
+        });
+    }
+
+    private void createUser(){
+
     }
 }
