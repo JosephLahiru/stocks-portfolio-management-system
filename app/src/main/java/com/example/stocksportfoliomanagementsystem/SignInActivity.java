2cd4f9ddf2e9a7ba5efcaf96d6c3576e00800fc9
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-09-17 14:26:53 +0530

    Added Sign In

---
+++ app/src/main/java/com/example/stocksportfoliomanagementsystem/SignInActivity.java
@@ -0,0 +1,13 @@
+package com.example.stocksportfoliomanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
