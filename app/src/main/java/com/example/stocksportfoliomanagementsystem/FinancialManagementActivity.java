4c9078b6c3d9b50696d7f94f154e6426275bb511
Niraj Nimantha Dilshan <nirajnimantha@gmail.com>
2022-10-19 16:22:49 +0530

    add-button-backtodashborad

--- app/src/main/java/com/example/stocksportfoliomanagementsystem/FinancialManagementActivity.java
+++ app/src/main/java/com/example/stocksportfoliomanagementsystem/FinancialManagementActivity.java
@@ -2,13 +2,27 @@ package com.example.stocksportfoliomanagementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FinancialManagementActivity extends AppCompatActivity {
+
+    Button backToDashboardButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_financial_management);
+
+        backToDashboardButton = (Button) findViewById(R.id.fmback);
+        backToDashboardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FinancialManagementActivity.this, MenuActivity.class));
+                finish();
+            }
+        });
     }
 }
