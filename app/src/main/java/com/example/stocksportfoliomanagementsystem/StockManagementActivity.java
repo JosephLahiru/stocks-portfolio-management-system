072f425476c3ddd63963bf2de2c86d652ae95100
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-10-19 19:32:40 +0530

    Finished AddStockActivity Created ViewStockHistoryActivity

--- app/src/main/java/com/example/stocksportfoliomanagementsystem/StockManagementActivity.java
+++ app/src/main/java/com/example/stocksportfoliomanagementsystem/StockManagementActivity.java
@@ -2,16 +2,28 @@ package com.example.stocksportfoliomanagementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StockManagementActivity extends AppCompatActivity {
+
+    Button addStockBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock_management);
 
+        addStockBtn = (Button) findViewById(R.id.addStocks);
 
-
+        addStockBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StockManagementActivity.this, AddStockActivity.class));
+                finish();
+            }
+        });
     }
 }
