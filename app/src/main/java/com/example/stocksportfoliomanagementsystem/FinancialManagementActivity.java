18436b7fcfe0cbcaf498eeddf5ac607aa43e227d
Niraj Nimantha Dilshan <nirajnimantha@gmail.com>
2022-10-19 14:55:46 +0530

    add-FinancialManagement

---
+++ app/src/main/java/com/example/stocksportfoliomanagementsystem/FinancialManagementActivity.java
@@ -0,0 +1,14 @@
+package com.example.stocksportfoliomanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FinancialManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_financial_management);
+    }
+}
