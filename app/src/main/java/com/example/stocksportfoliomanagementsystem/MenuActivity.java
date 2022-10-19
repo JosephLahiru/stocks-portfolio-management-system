5c18fa2a4c8bdda2dd18af767360ba6bd1d036e7
Niraj Nimantha Dilshan <nirajnimantha@gmail.com>
2022-10-19 15:58:24 +0530

    add-buttons-to-FM

--- app/src/main/java/com/example/stocksportfoliomanagementsystem/MenuActivity.java
+++ app/src/main/java/com/example/stocksportfoliomanagementsystem/MenuActivity.java
@@ -2,13 +2,28 @@ package com.example.stocksportfoliomanagementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+    Button financeManagementButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        financeManagementButton = (Button) findViewById(R.id.financialMngButton);
+
+        financeManagementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MenuActivity.this, FinancialManagementActivity.class));
+                finish();
+            }
+        });
     }
 }
