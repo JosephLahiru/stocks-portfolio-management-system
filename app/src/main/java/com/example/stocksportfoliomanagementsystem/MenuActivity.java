3a04a0251cf140be864526a6884faa899003d22e
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-10-19 15:58:30 +0530

    Linked StockManagement to menu activity

--- app/src/main/java/com/example/stocksportfoliomanagementsystem/MenuActivity.java
+++ app/src/main/java/com/example/stocksportfoliomanagementsystem/MenuActivity.java
@@ -2,13 +2,28 @@ package com.example.stocksportfoliomanagementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+    Button stockManagementButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        stockManagementButton = (Button) findViewById(R.id.stockMngButton);
+
+        stockManagementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MenuActivity.this, StockManagementActivity.class));
+                finish();
+            }
+        });
     }
 }
