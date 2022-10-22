00356157d90a49642ac937e4146c7f11db92331e
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-10-22 22:38:09 +0530

    Added and Finished DeleteStockActivity and UpdateStockActivity

--- app/src/main/java/com/example/stocksportfoliomanagementsystem/UpdateStockActivity.java
+++ app/src/main/java/com/example/stocksportfoliomanagementsystem/UpdateStockActivity.java
@@ -10,27 +10,30 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import de.codecrafters.tableview.TableView;
 import de.codecrafters.tableview.toolkit.SimpleTableDataAdapter;
 import de.codecrafters.tableview.toolkit.SimpleTableHeaderAdapter;
 
-public class ViewStockHistoryActivity extends AppCompatActivity {
+public class UpdateStockActivity extends AppCompatActivity {
 
     TableView tableView;
     Connection connection;
     String data[][];
-    Button backBtn;
+    Button backBtn, updateBtn;
+
+    EditText updateDataID, updateDataColumn, newData;
 
     private static final String URL = "jdbc:mysql://152.70.158.151:3306/spms";
     private static final String USER = "root";
@@ -39,24 +42,83 @@ public class ViewStockHistoryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_stock_history);
+        setContentView(R.layout.activity_update_stock);
 
-        backBtn = (Button) findViewById(R.id.backButton2);
+        updateDataID = (EditText) findViewById(R.id.etStockIDUpdate);
+        updateDataColumn = (EditText) findViewById(R.id.etStockColumnUpdate);
+        newData = (EditText) findViewById(R.id.etNewValue);
+
+        backBtn = (Button) findViewById(R.id.backButton4);
+        updateBtn = (Button) findViewById(R.id.updateButton);
+
+        updateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new UpdateStockTask().execute();
+
+                updateDataID.getText().clear();
+                updateDataColumn.getText().clear();
+                newData.getText().clear();
+            }
+        });
 
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(ViewStockHistoryActivity.this, StockManagementActivity.class));
+                startActivity(new Intent(UpdateStockActivity.this, StockManagementActivity.class));
                 finish();
             }
         });
 
-        tableView = findViewById(R.id.table_data_view);
+        tableView = findViewById(R.id.table_data_view_update);
         String headers[] = {"PS ID", "Arrived Date", "Stock Qty", "Product ID"};
 
         tableView.setHeaderAdapter(new SimpleTableHeaderAdapter(this, headers));
 
         new InfoAsyncTask().execute();
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    public class UpdateStockTask extends AsyncTask<Void, Void, List<String>> {
+        @Override
+        protected List<String> doInBackground(Void... voids) {
+            List<String> products = new ArrayList<>();
+            try {
+
+                String stockID = updateDataID.getText().toString();
+                String stockColumn = updateDataColumn.getText().toString();
+                String stockNewData = newData.getText().toString();
+
+                String sql = "UPDATE product_stock SET `" + stockColumn + "` = '" + stockNewData + "' WHERE  `product_stock_id` = '" + stockID + "'; ";
+
+                System.out.println(sql);
+
+                Statement st1 = connection.createStatement();
+
+                if (!st1.execute(sql)) {
+                    System.out.println("Data updated successfully");
+
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            final Toast toast = Toast.makeText(UpdateStockActivity.this, "Data updated Successfully.", Toast.LENGTH_SHORT);
+                            toast.show();
+                        }
+                    });
+
+                } else {
+                    System.out.println("Data upload failed.");
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            final Toast toast = Toast.makeText(UpdateStockActivity.this, "Data Upload Failed.", Toast.LENGTH_SHORT);
+                            toast.show();
+                        }
+                    });
+                }
+            } catch (Exception e) {
+                Log.e("InfoAsyncTask", "Error reading information", e);
+            }
+            return products;
+        }
     }
 
     @SuppressLint("StaticFieldLeak")
@@ -98,7 +160,7 @@ public class ViewStockHistoryActivity extends AppCompatActivity {
 
 
             if (!result.isEmpty()) {
-                tableView.setDataAdapter(new SimpleTableDataAdapter(ViewStockHistoryActivity.this, arr));
+                tableView.setDataAdapter(new SimpleTableDataAdapter(UpdateStockActivity.this, arr));
             }
         }
     }
