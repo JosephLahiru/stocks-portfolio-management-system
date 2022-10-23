024adc0b1c3df8a0fb23ac785d1a434d3b9a108f
Niraj Nimantha Dilshan <nirajnimantha@gmail.com>
2022-10-23 23:03:25 +0530

    update-coding-transaction-add

--- app/src/main/java/com/example/stocksportfoliomanagementsystem/ViewTransactionsActivity.java
+++ app/src/main/java/com/example/stocksportfoliomanagementsystem/ViewTransactionsActivity.java
@@ -11,14 +11,12 @@ import android.os.AsyncTask;
 import android.util.Log;
 import android.annotation.SuppressLint;
 import android.os.Build;
-import android.widget.ArrayAdapter;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import de.codecrafters.tableview.TableView;
