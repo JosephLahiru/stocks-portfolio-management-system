0a1bf4326819b48234e7167794afad89cbe9908b
Niraj Nimantha Dilshan <nirajnimantha@gmail.com>
2022-10-26 20:46:52 +0530

    supplier-management-activities

--- app/src/main/java/com/example/stocksportfoliomanagementsystem/SupplierManagementActivity.java
+++ app/src/main/java/com/example/stocksportfoliomanagementsystem/SupplierManagementActivity.java
@@ -3,8 +3,22 @@ package com.example.stocksportfoliomanagementsystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SupplierManagementActivity extends AppCompatActivity {
+
+    Button backToDashboardButton;
+    Button viewRequiredSupplyDetailsButton;
+    Button viewSupplyDetailsButton;
+    Button saveSupplyDetailsButton;
+    Button updateSupplyDetailsButton;
+    Button deleteSupplyDetailsButton;
+    Button viewDiscountsDetailsButton;
+    Button addDiscountsDetailsButton;
+    Button updateDiscountsDetailsButton;
+    Button deleteDiscountsDetailsButton;
+    Button updateUserDetailsButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
