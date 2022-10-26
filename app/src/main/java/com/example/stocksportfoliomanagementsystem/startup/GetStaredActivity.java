0e492b788ef752d2c1d2742dc9597bb9ca9a958e
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-10-26 21:36:21 +0530

    moved and sorted files

--- app/src/main/java/com/example/stocksportfoliomanagementsystem/startup/GetStaredActivity.java
+++ app/src/main/java/com/example/stocksportfoliomanagementsystem/startup/GetStaredActivity.java
@@ -1,4 +1,4 @@
-package com.example.stocksportfoliomanagementsystem;
+package com.example.stocksportfoliomanagementsystem.startup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.stocksportfoliomanagementsystem.R;
 
 public class GetStaredActivity extends AppCompatActivity {
 
