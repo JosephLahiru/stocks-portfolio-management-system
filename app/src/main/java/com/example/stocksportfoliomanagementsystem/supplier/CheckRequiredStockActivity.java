ff9e85fc560b2fa3873b72df396c73ab08e4fa6d
Niraj Nimantha Dilshan <nirajnimantha@gmail.com>
2022-10-27 20:30:32 +0530

    Check-Required-Stock-complete

--- app/src/main/java/com/example/stocksportfoliomanagementsystem/supplier/CheckRequiredStockActivity.java
+++ app/src/main/java/com/example/stocksportfoliomanagementsystem/supplier/CheckRequiredStockActivity.java
@@ -1,16 +1,103 @@
 package com.example.stocksportfoliomanagementsystem.supplier;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.stocksportfoliomanagementsystem.R;
+import com.example.stocksportfoliomanagementsystem.transactions.ViewTransactionsActivity;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import de.codecrafters.tableview.TableView;
+import de.codecrafters.tableview.toolkit.SimpleTableDataAdapter;
+import de.codecrafters.tableview.toolkit.SimpleTableHeaderAdapter;
 
 public class CheckRequiredStockActivity extends AppCompatActivity {
+
+    TableView tableView;
+    Button backToFinancialManagementButton;
+    Connection connection;
+    String data[][];
+
+    private static final String URL = "jdbc:mysql://152.70.158.151:3306/spms";
+    private static final String USER = "root";
+    private static final String PASSWORD = "amres";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_required_stock);
+        backToFinancialManagementButton = (Button) findViewById(R.id.vbsm);
+        backToFinancialManagementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CheckRequiredStockActivity.this, SupplierManagementActivity.class));
+                finish();
+            }
+        });
+
+        tableView = findViewById(R.id.tableViewActivityCheckRequiredStock);
+        tableView.setColumnCount(2);
+        String headers[] = {"Product Name", "Required Quantity"};
+        tableView.setHeaderAdapter(new SimpleTableHeaderAdapter(this, headers));
+        new InfoAsyncTask().execute();
+
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    public class InfoAsyncTask extends AsyncTask<Void, Void, List<List<String>>> {
+        @Override
+        protected List<List<String>> doInBackground(Void... voids) {
+            List<List<String>> products = new ArrayList<>();
+            try {
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+
+                String sql = "SELECT product.product_name, stock_required.stock_needed FROM product CROSS JOIN stock_required WHERE product.product_id=stock_required.product_id;";
+                PreparedStatement statement = connection.prepareStatement(sql);
+                ResultSet resultSet = statement.executeQuery();
+
+                while (resultSet.next()) {
+                    List<String> temp = new ArrayList<>();
+                    temp.add(resultSet.getString("product_name"));
+                    temp.add(resultSet.getString("stock_needed"));
+                    products.add(temp);
+                }
+            } catch (Exception e) {
+                Log.e("InfoAsyncTask", "Error reading information", e);
+            }
+
+            return products;
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.N)
+        @Override
+        protected void onPostExecute(List<List<String>> result) {
+
+            //String[][] data = {{"1", "2", "3", "4"},{"5", "6", "7", "8"}};
+
+            String[][] arr = result.stream()
+                    .map(l -> l.stream().toArray(String[]::new))
+                    .toArray(String[][]::new);
+
+
+            if (!result.isEmpty()) {
+                tableView.setDataAdapter(new SimpleTableDataAdapter(CheckRequiredStockActivity.this, arr));
+            }
+
+        }
     }
 }
