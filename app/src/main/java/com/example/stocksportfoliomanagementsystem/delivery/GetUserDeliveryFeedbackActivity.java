41d9d84c1dbb51435604bd649a55ab22906abdde
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-10-31 19:38:06 +0530

    Created GetUserDeliveryFeedbackActivity

---
+++ app/src/main/java/com/example/stocksportfoliomanagementsystem/delivery/GetUserDeliveryFeedbackActivity.java
@@ -0,0 +1,15 @@
+package com.example.stocksportfoliomanagementsystem.delivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.stocksportfoliomanagementsystem.R;
+
+public class GetUserDeliveryFeedbackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_user_delivery_feedback);
+    }
+}
