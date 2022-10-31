0c4a035f79fbc1bb573c9799daaa4429b6a94d84
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-10-31 22:10:21 +0530

    Created CreateCustomerActivity

---
+++ app/src/main/java/com/example/stocksportfoliomanagementsystem/customer/CreateCustomerActivity.java
@@ -0,0 +1,15 @@
+package com.example.stocksportfoliomanagementsystem.customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.stocksportfoliomanagementsystem.R;
+
+public class CreateCustomerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_customer);
+    }
+}
