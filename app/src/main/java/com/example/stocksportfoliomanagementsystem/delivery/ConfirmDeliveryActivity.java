90d056a6d08d840432a9c003157eae32d1706a38
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-10-31 19:35:22 +0530

    Created ConfirmDeliveryActivity

---
+++ app/src/main/java/com/example/stocksportfoliomanagementsystem/delivery/ConfirmDeliveryActivity.java
@@ -0,0 +1,15 @@
+package com.example.stocksportfoliomanagementsystem.delivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.stocksportfoliomanagementsystem.R;
+
+public class ConfirmDeliveryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_delivery);
+    }
+}
