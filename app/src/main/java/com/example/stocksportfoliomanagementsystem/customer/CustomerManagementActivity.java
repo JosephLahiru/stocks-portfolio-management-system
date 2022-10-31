514e62240d2a47e7154e2eea6b28f85917008706
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-10-31 23:39:16 +0530

    Fixed the flow from CustomerManagementActivity to MenuActivity

--- app/src/main/java/com/example/stocksportfoliomanagementsystem/customer/CustomerManagementActivity.java
+++ app/src/main/java/com/example/stocksportfoliomanagementsystem/customer/CustomerManagementActivity.java
@@ -8,15 +8,25 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.stocksportfoliomanagementsystem.R;
+import com.example.stocksportfoliomanagementsystem.startup.MenuActivity;
 
 public class CustomerManagementActivity extends AppCompatActivity {
 
-    Button createCustomerBtn;
+    Button createCustomerBtn, backToMenuBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_management);
+
+        backToMenuBtn = (Button) findViewById(R.id.backToMenu12);
+
+        backToMenuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CustomerManagementActivity.this, MenuActivity.class));
+            }
+        });
 
         createCustomerBtn = (Button) findViewById(R.id.createCustomer);
 
