fa58f998bbde33e900fdc9e4b5cea1790728018b
Niraj Nimantha Dilshan <nirajnimantha@gmail.com>
2022-10-31 06:41:07 +0530

    add-discounts-Completed

--- app/src/main/java/com/example/stocksportfoliomanagementsystem/supplier/AddDiscountsDetailsActivity.java
+++ app/src/main/java/com/example/stocksportfoliomanagementsystem/supplier/AddDiscountsDetailsActivity.java
@@ -2,15 +2,161 @@ package com.example.stocksportfoliomanagementsystem.supplier;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.stocksportfoliomanagementsystem.R;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddDiscountsDetailsActivity extends AppCompatActivity {
+
+    String userEmail;
+    Spinner dropdown;
+    Button addDiscountsButton,backToSupplireManagementButton;
+    Connection connection;
+    EditText margin,discount;
+    int supplier_id;
+
+    private static final String URL = "jdbc:mysql://152.70.158.151:3306/spms";
+    private static final String USER = "root";
+    private static final String PASSWORD = "amres";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_discounts_details);
+
+        userEmail = getIntent().getStringExtra("userEmail");
+
+        dropdown = findViewById(R.id.spinneraddsupplierdiscounts);
+
+        margin = (EditText) findViewById(R.id.editTextMarginASD);
+        discount = (EditText) findViewById(R.id.editTextDiscountASD);
+
+        new InfoAsyncTask().execute();
+
+        addDiscountsButton = (Button) findViewById(R.id.btnadddiscountsASD);
+        addDiscountsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AddDiscountsTask().execute();
+            }
+        });
+
+        backToSupplireManagementButton = (Button) findViewById(R.id.vbsm7);
+        backToSupplireManagementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddDiscountsDetailsActivity.this, SupplierManagementActivity.class);
+                intent.putExtra("userEmail", userEmail);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    public class InfoAsyncTask extends AsyncTask<Void, Void, List<String>> {
+        @Override
+        protected List<String> doInBackground(Void... voids) {
+            List<String> products = new ArrayList<>();
+            try {
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                String sql = "SELECT * FROM product";
+                PreparedStatement statement = connection.prepareStatement(sql);
+                ResultSet resultSet = statement.executeQuery();
+                while (resultSet.next()) {
+                    products.add(resultSet.getString("product_id") + " - " + resultSet.getString("product_name"));
+                }
+            } catch (Exception e) {
+                Log.e("InfoAsyncTask", "Error reading information", e);
+            }
+
+            return products;
+        }
+
+        @Override
+        protected void onPostExecute(List<String> result) {
+            if (!result.isEmpty()) {
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(AddDiscountsDetailsActivity.this,
+                        android.R.layout.simple_spinner_item, result);
+
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                dropdown.setAdapter(adapter);
+            }
+        }
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    public class AddDiscountsTask extends AsyncTask<Void, Void, List<String>> {
+        @Override
+        protected List<String> doInBackground(Void... voids) {
+            List<String> products = new ArrayList<>();
+            try {
+
+                String query = "SELECT supplier_details.supplier_id  FROM user CROSS JOIN supplier_details WHERE user.user_email='"+userEmail+"' and supplier_details.user_id = user.user_id;";
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+                ResultSet resultSet2 = preparedStatement.executeQuery();
+
+                while (resultSet2.next()){
+                    supplier_id = resultSet2.getInt("supplier_id");
+                    System.out.println(supplier_id);
+                }
+
+                String productId = dropdown.getSelectedItem().toString().split("-")[0];
+                System.out.println(productId);
+
+                String margin_ = margin.getText().toString();
+                String discount_ = discount.getText().toString();
+
+                String sql = "INSERT INTO `spms`.`supplier_discounts` (`product_id`, `supplier_id`, `margin`, `discount`) VALUES ('"+productId+"', '"+supplier_id+"', '"+margin_+"', '"+discount_+"');";
+
+                System.out.println(sql);
+
+                Statement st1 = connection.createStatement();
+
+                if (!st1.execute(sql)) {
+                    System.out.println("Data uploaded successfully");
+
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            final Toast toast = Toast.makeText(AddDiscountsDetailsActivity.this, "Data Uploaded Successfully.", Toast.LENGTH_SHORT);
+                            toast.show();
+                        }
+                    });
+
+                    margin.getText().clear();
+                    discount.getText().clear();
+
+                } else {
+                    System.out.println("Data upload failed.");
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            final Toast toast = Toast.makeText(AddDiscountsDetailsActivity.this, "Data Upload Failed.", Toast.LENGTH_SHORT);
+                            toast.show();
+                        }
+                    });
+                }
+            } catch (Exception e) {
+                Log.e("InfoAsyncTask", "Error reading information", e);
+            }
+            return products;
+        }
     }
 }
