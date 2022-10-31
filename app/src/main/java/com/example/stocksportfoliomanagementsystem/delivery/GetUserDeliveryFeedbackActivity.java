f12d0c738f249b1d13be01f64d45d5e847e192fd
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-10-31 21:09:36 +0530

    Finished GetUserDeliveryFeedbackActivity

--- app/src/main/java/com/example/stocksportfoliomanagementsystem/delivery/GetUserDeliveryFeedbackActivity.java
+++ app/src/main/java/com/example/stocksportfoliomanagementsystem/delivery/GetUserDeliveryFeedbackActivity.java
@@ -1,15 +1,100 @@
 package com.example.stocksportfoliomanagementsystem.delivery;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 import com.example.stocksportfoliomanagementsystem.R;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import de.codecrafters.tableview.TableView;
+import de.codecrafters.tableview.toolkit.SimpleTableDataAdapter;
+import de.codecrafters.tableview.toolkit.SimpleTableHeaderAdapter;
+
 public class GetUserDeliveryFeedbackActivity extends AppCompatActivity {
+
+    TableView tableView;
+    Connection connection;
+    Button backBtn;
+
+    private static final String URL = "jdbc:mysql://152.70.158.151:3306/spms";
+    private static final String USER = "root";
+    private static final String PASSWORD = "amres";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_user_delivery_feedback);
+
+        backBtn = (Button) findViewById(R.id.backButtonDeliveryFeedback);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(GetUserDeliveryFeedbackActivity.this, DeliveryManagementActivity.class));
+                finish();
+            }
+        });
+
+        tableView = findViewById(R.id.table_data_view_delivery_feedback);
+        String headers[] = {"Feedback ID", "Feedback"};
+
+        tableView.setHeaderAdapter(new SimpleTableHeaderAdapter(this, headers));
+
+        new InfoAsyncTask().execute();
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    public class InfoAsyncTask extends AsyncTask<Void, Void, List<List<String>>> {
+        @Override
+        protected List<List<String>> doInBackground(Void... voids) {
+            List<List<String>> products = new ArrayList<>();
+            try {
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+
+                String sql = "SELECT * FROM customer_delivery_feedback";
+                PreparedStatement statement = connection.prepareStatement(sql);
+                ResultSet resultSet = statement.executeQuery();
+
+                while (resultSet.next()) {
+                    List<String> temp = new ArrayList<>();
+                    temp.add(resultSet.getString("df_id"));
+                    temp.add(resultSet.getString("feedback"));
+                    products.add(temp);
+                }
+            } catch (Exception e) {
+                Log.e("InfoAsyncTask", "Error reading information", e);
+            }
+
+            return products;
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.N)
+        @Override
+        protected void onPostExecute(List<List<String>> result) {
+
+            String[][] arr = result.stream()
+                    .map(l -> l.stream().toArray(String[]::new))
+                    .toArray(String[][]::new);
+
+
+            if (!result.isEmpty()) {
+                tableView.setDataAdapter(new SimpleTableDataAdapter(GetUserDeliveryFeedbackActivity.this, arr));
+            }
+        }
     }
 }
