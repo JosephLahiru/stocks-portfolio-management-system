4983e6c3ac904a269fa43c46083f65108f008e60
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-10-31 21:57:46 +0530

    Created CustomerManagementActivity

---
+++ app/src/main/java/com/example/stocksportfoliomanagementsystem/customer/CustomerManagementActivity.java
@@ -0,0 +1,15 @@
+package com.example.stocksportfoliomanagementsystem.customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.stocksportfoliomanagementsystem.R;
+
+public class CustomerManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_management);
+    }
+}
